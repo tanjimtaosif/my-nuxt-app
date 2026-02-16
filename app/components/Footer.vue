faeca202defb882f0e51723463861acb25184936
tanjimtaosif <itsmahtab007@gmail.com>
2026-02-16 21:13:55 +0600

    Refactor footer component with SCSS styling and updated copyright text

--- app/components/Footer.vue
+++ app/components/Footer.vue
@@ -1,7 +1,19 @@
 <template>
-  <footer>
-    <div>
-      <p>&copy; 2026 My Nuxt App. All rights reserved.</p>
-    </div>
+  <footer class="site_footer_component">
+    <p class="site_footer_component__text">&copy; 2026 Nuxt Lab. All rights reserved.</p>
   </footer>
 </template>
+
+<style lang="scss" scoped>
+.site_footer_component {
+  padding: var(--space_lg);
+  text-align: center;
+  border-top: 1px solid var(--color_border);
+  margin-top: var(--space_xl);
+
+  &__text {
+    font-size: var(--font_size_sm);
+    color: var(--color_text_light);
+  }
+}
+</style>

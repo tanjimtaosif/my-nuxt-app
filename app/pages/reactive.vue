03948c33842cae35634800e3853f3df1e16c7094
tanjimtaosif <itsmahtab007@gmail.com>
2026-02-16 16:25:18 +0600

    Add reactive component for cart functionality with user input

---
+++ app/pages/reactive.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { ref, computed, watch, reactive } from "vue";
+
+const name = ref("");
+const cart = reactive({
+  items: [],
+  total: 0,
+});
+
+const isBigCart = computed(() => cart.items.length >= 3);
+
+watch(name, (newVal, oldVal) => {
+  console.log(`Name changed from ${oldVal} to ${newVal}`);
+});
+
+function addItem() {
+  cart.items.push("item");
+  cart.total += 10;
+}
+</script>
+
+<template>
+  <div>
+    <input v-model="name" type="text" placeholder="Enter your name" />
+    <p>Hello, {{ name }}</p>
+    <p>Cart: {{ cart.items.length }} items: (total: ${{ cart.total }})</p>
+    <button @click="addItem">Add items</button>
+    <p v-if="isBigCart">You have a big cart</p>
+  </div>
+</template>

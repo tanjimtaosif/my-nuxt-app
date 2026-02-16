775b2b68d2977dc0678b20cb110d6c068efbca93
tanjimtaosif <itsmahtab007@gmail.com>
2026-02-16 20:11:35 +0600

    Add life cycle hooks example component with count functionality

---
+++ app/pages/life-cycle.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref, onMounted, onUpdated, onBeforeUnmount } from "vue";
+
+const count = ref(0);
+
+onMounted(() => {
+  console.log("Component Mounted!");
+});
+
+onUpdated(() => {
+  console.log("Component Updated!");
+});
+
+onUnmounted(() => {
+  console.log("Component Unmounted!");
+});
+
+function increment() {
+  count.value++;
+}
+</script>
+
+<template>
+  <div>
+    <h2>Life Cycle Hooks</h2>
+    <p>Count: {{ count }}</p>
+    <button @click="increment">Increatment</button>
+  </div>
+</template>

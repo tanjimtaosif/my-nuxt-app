9cd47ee7fb19df87dbea01a9e8ce02d57a411010
tanjimtaosif <itsmahtab007@gmail.com>
2026-02-16 20:22:28 +0600

    Add compose.vue for counter functionality using composable

---
+++ app/pages/compose.vue
@@ -0,0 +1,12 @@
+<script setup>
+const { count, increment, decrement } = useCounter();
+</script>
+<template>
+  <div>
+    <h2>Counter using composable</h2>
+    <p>Count: {{ count }}</p>
+    <button @click="increment">Increatment</button>
+    <button @click="decrement">Increatment</button>
+  </div>
+  <NuxtLink to="/">Back to home</NuxtLink>
+</template>

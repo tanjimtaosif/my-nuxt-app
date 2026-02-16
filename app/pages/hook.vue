8aa3e336d8040fa5e4fbb7c8577123befef4e07a
tanjimtaosif <itsmahtab007@gmail.com>
2026-02-16 20:11:25 +0600

    Add hook page with async data fetching and Nuxt readiness log

---
+++ app/pages/hook.vue
@@ -0,0 +1,16 @@
+<script setup>
+const { data } = await useAsyncData("posts", () =>
+  $fetch("https://jsonplaceholder.typicode.com/posts/1"),
+);
+
+onNuxtReady(() => {
+  console.log("Nuxt app is ready");
+});
+</script>
+
+<template>
+  <div>
+    <h2>Hook</h2>
+    <p>{{ data }}</p>
+  </div>
+</template>

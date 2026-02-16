85faf09d7db32e49733699e6a805feec3c79f9f5
tanjimtaosif <itsmahtab007@gmail.com>
2026-02-16 13:11:56 +0600

    Add blogs index page with navigation links

---
+++ app/pages/blogs/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h2>Blogs</h2>
+    <NuxtLink to="/blogs/javascript">JavaScript</NuxtLink> <br />
+    <NuxtLink to="/blogs/nuxt">Nuxt JS</NuxtLink>
+    <br />
+    <br />
+    <NuxtLink to="/">Go to Home</NuxtLink>
+  </div>
+</template>

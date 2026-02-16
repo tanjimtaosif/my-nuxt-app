7e7b9a3718e9c1851b1ce4cc111eee254105d63b
tanjimtaosif <itsmahtab007@gmail.com>
2026-02-16 21:39:35 +0600

    Add font preconnect and stylesheet links for improved performance and styling

--- app/app.vue
+++ app/app.vue
@@ -7,6 +7,11 @@ useHead({
   htmlAttrs: {
     lang: 'en',
   },
+  link: [
+    { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+    { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' },
+    { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap' },
+  ],
 });
 </script>
 

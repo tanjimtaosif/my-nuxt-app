61a1ef15d11db80afea05dfea56b4e2caee989dc
tanjimtaosif <itsmahtab007@gmail.com>
2026-02-16 13:11:46 +0600

    Add dynamic blog and product pages with navigation links

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,14 +1,13 @@
 <template>
-    <div>
-        <h2>Home Page</h2>
-        <NuxtLink to="/contact">Contact Us</NuxtLink> <br>
-        <NuxtLink to="/products">Products</NuxtLink> <br>
+  <div>
+    <h2>Home Page</h2>
+    <NuxtLink to="/contact">Contact Us</NuxtLink> <br />
+    <NuxtLink to="/products">Products</NuxtLink> <br />
+    <NuxtLink to="/blogs">Blogs</NuxtLink> <br />
 
-        <NuxtLink to="/products/1">Dynamic 1</NuxtLink> <br>
-        <NuxtLink to="/products/2">Dynamic 2</NuxtLink> <br>
-        <NuxtLink to="/products/3">Dynamic 3</NuxtLink> <br>
-
-
-
-    </div>
+    <br />
+    <NuxtLink to="/products/1">Dynamic 1</NuxtLink> <br />
+    <NuxtLink to="/products/2">Dynamic 2</NuxtLink> <br />
+    <NuxtLink to="/products/3">Dynamic 3</NuxtLink> <br />
+  </div>
 </template>

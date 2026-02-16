a33e7eb3fcab31cdd4d66aad1834e30272d6d0d5
tanjimtaosif <itsmahtab007@gmail.com>
2026-02-16 13:11:53 +0600

    Add 404 error page for blog routes with navigation link

---
+++ app/pages/blogs/[...slug].vue
@@ -0,0 +1,12 @@
+<script setup>
+const route = useRoute();
+</script>
+
+<template>
+  <h2>Oops! page not found</h2>
+
+  <p>
+    You tried to access: <strong>{{ route.params.slug.join("/") }}</strong>
+  </p>
+  <p>Go back to <NuxtLink to="/blogs">Blogs Page</NuxtLink></p>
+</template>
